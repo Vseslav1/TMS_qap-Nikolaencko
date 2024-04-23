2d686ba6976d9994c6934ff0decdc00c6acb8081
Vseslav Nikolaenko <vseslavnikol@gmail.com>
2024-04-23 17:15:12 +0300

    QAP_18_HW3

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print('hello world')
+# print('hello world')
+#
+# print('hello world')
 
-print('hello world')
+name = "vseslav"
+surname = "nikoaenko"
+age = 26
+
+print(name,surname,age)
+
+name = 'JohnSnow'
+age = 29
+
+print(name,age)
