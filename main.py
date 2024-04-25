e0cc0b02afeaddc3e671a8d15f35b381e1be7ab8
Vseslav Nikolaenko <vseslavnikol@gmail.com>
2024-04-25 10:25:10 +0300

    d32full

--- main.py
+++ main.py
@@ -31,29 +31,96 @@
 # print(c)
 
 # 3 Напишите программу, которая добавляет ‘ing’ к слову ‘stroka’
-
-# s1 = 'stroka'
-# s2 = 'ing'
 #
-# print(s1+s2)
+# text = ['stroka','ing']
+#
+# resultat = ''.join(text)
+#
+# print(resultat)
+
+
 
 # 4 В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
+
+# a = 'Ivanou Ivan'
+#
+# b = a.split()
+#
+# c = ['Ivanov','Ivan']
+#
+# c.reverse()
+#
+# b = ' '.join(c)
+#
+# print(b)
+
+
+
+
+
 # 5 Напишите программу которая удаляет пробел в начале, в конце строки
+
+# text = " Напишите программу которая удаляет пробел в начале, в конце строки "
+# text = text.strip()
+#
+# print(text)
+
 # 6 Создайте словарь, связав его с переменной school, и наполните его данными которые бы отражали количество учащихся
 # в десяти разных классах (например, 1а, 1б, 2б, 6а, 7в и т.д.).
+
+# dict = 'school'
+#
+#
+# school =[
+#     {'class': '1A','number of students': '23'},
+#     {'class': '2A','number of students': '33'},
+#     {'class': '3A','number of students': '21'},
+#     {'class': '4A','number of students': '19'},
+#     {'class': '5A','number of students': '11'},
+#     {'class': '1b','number of students': '22'},
+#     {'class': '1c','number of students': '24'},
+#     {'class': '4c','number of students': '18'},
+#     {'class': '11a','number of students': '25'},
+#     {'class': '9b','number of students': '14'},
+#     {'class': '10b','number of students': '17'}
+# ]
+#
+# print(school[10].keys())
+
 # 7 Создайте список и извлеките из него списка второй элемент.
+
+# list_mix =[1,'2',3,4,5,6,False]
+# #
+# #
+# print(list_mix[1])
+
 # 8 Вывести входит ли строка1 в строку2 (пример: employ и employment )
+
+# str1 = 'стол'
+# strochka2 = 'столешница'
+# if strochka2.find('стол') == 0:
+#     print('yes')
+
 # 9 Вывести нужные символы
 # x = "My name is Agent Smith"
-# print(x[?]) #y
-# print(x[?:?:?]) #nesgt
+
+# print(x[1]) #y
+
+# print(x[3],x[6],x[9],x[12],x[15])  #nesgt
+
 # 10 Есть массив чисел. Известно, что каждое число в этом массиве имеет пару, кроме одного: [1, 5, 2, 9, 2, 9, 1] => 5
 # Напишите программу, котораяпппппп будет выводить уникальное число
 
 
+# list1 = [1, 5, 2, 9, 2, 9, 1]
+# a = []
+# for i in list1:
+#     if list1.count(i) == 1: #количество вхождений
+#         a.append(i)
+# print(a)
 
 
-# задание с урока
+# задание с ур
 
 # text1 = "HEllO WORLD"
 # text2 = " "
@@ -62,3 +129,5 @@
 # sum = text1 + text2 + text3
 
 # print(sum)
+
+
