675ff955131d710d1c414faa4b77458154b289af
Vseslav Nikolaenko <vseslavnikol@gmail.com>
2024-04-29 22:22:11 +0300

    ideadell

---
+++ hhh.py
@@ -0,0 +1,17 @@
+# def summ_num(a ,b) -> int:
+#     """
+#      Return sum of 2 numbers
+#     :param a: first number
+#     :param b: second numder
+#     :return: sum of two numbers
+#     """
+#     return a + b
+#
+#
+# print(summ_num(a=12,b=14))
+
+list1 = ['один', 'два', 'три', 'четыре']
+
+new_list = list(map(len,list1))
+
+print(new_list)
